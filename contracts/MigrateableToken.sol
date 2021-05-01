0dc9b703f2cfb38f8337ca783f0a9a5da0989d81
chainvisions <chainvisions@outlook.com>
2021-05-01 11:19:15 -0600

    Fix errors

--- contracts/MigrateableToken.sol
+++ contracts/MigrateableToken.sol
@@ -1,16 +1,17 @@
 pragma solidity 0.5.16;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownabke.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract MigrateableToken is ERC20, Ownable {
+contract MigrateableToken is ERC20, ERC20Detailed, Ownable {
     address public migrator;
     bool public migratorInitialized;
 
     constructor(
         string memory _name, 
         string memory _symbol
-    ) public ERC20(_name, _symbol, 18) {}
+    ) public ERC20Detailed(_name, _symbol, 18) {}
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
